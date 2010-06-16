83974f1cda3e3507504db22382f8798cb71f20cb
jonathan.p.newman <jonathan.p.newman@61528c3a-0038-4838-3383-300b820175fd>
2010-06-16 22:52:16 +0000

    -Partially working waveclus implementation - Will be fully functional soon with a large number of files pruned.

---
+++ SSTestScript.m
@@ -0,0 +1,19 @@
+% Load data into data structs
+spkdat = loadspike('20100604_16269_stim.spk');
+stimdat = loadstim('20100604_16269_stim.stim');
+spontdat = loadspike('20100604_16269_spont.spk');
+
+% Instantiate a SqueakySpk Object
+SStest = SqueakySpk(spkdat);
+
+% Clustering
+SStest.WaveClus()
+
+% Remove spikes with blanks
+SStest.RemoveSpkWithBlank();
+
+% Perform a hard p2p cut at 175 uV
+SStest.HardThreshold(175);
+
+% Examime some data to make sure results of sorting and cleaning look good
+SStest.RasterWave_Comp([100 150]);
