301e35dbe020580aa8f0aef7d1a7a79ea7a5f671
jonathan.p.newman <jonathan.p.newman@61528c3a-0038-4838-3383-300b820175fd>
2011-06-15 01:37:35 +0000

    - Made some properties of the data server public so CL protocols could play with them

--- trunk/@SqueakySpk/Save.m
+++ trunk/@SqueakySpk/Save.m
@@ -1,23 +1,34 @@
-%% Block 8: Save SS object
 function Save(SS,auxfid)
 % Save(SS,auxfid) Save your SS object. auxfid is an optional argument
-% (string) specifying the fully quallified path and file name of your SS
+% (string) specifying the fully qualified path and file name of your SS
 % object. If no extension is specified, then the file will be save as *.SS.
 
 if nargin < 2 || isempty(auxfid)
     save([SS.name '.SS'],'SS')
 else
     if ~ischar(auxfid)
-        warning('your auxiliary file name is incorrectly formatted.');
+        warning('Your auxiliary file name is incorrectly formatted.');
         return
     end
     
     % find if there is an extension
     perloc = strfind(auxfid,'.');
-        
-    if isempty(perloc)
-         save([auxfid '.SS'],'SS');
+    
+    % Matlab versioning
+    v = version;
+    v = str2double(v(1:3));
+    
+    if v >= 7.3
+        if isempty(perloc)
+            save([auxfid '.SS'],'SS', '-V7.3');
+        else
+            save(auxfid,'SS', '-V7.3');
+        end
     else
-        save([auxfid],'SS');
+        if isempty(perloc)
+            save([auxfid '.SS'],'SS');
+        else
+            save(auxfid,'SS');
+        end
     end
 end
