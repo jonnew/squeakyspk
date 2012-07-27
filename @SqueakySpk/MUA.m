a1f2844364e9ea2f50791b827a817b74caf18a63
jonathan.p.newman <jonathan.p.newman@61528c3a-0038-4838-3383-300b820175fd>
2012-07-27 01:29:01 +0000

    -Starting info code branch for chr2 paper

--- @SqueakySpk/MUA.m
+++
@@ -1,7 +0,0 @@
-function MUA(SS)
-% MUA(SS) all units on the same channel are combined to create
-% Multi-Unit Activity (MUA)
-% Written by: NK
-SS.unit = SS.clean.*SS.channel;
-SS.methodlog = [SS.methodlog '<MUA>'];
-end
